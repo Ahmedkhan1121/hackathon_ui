ee1fc44540e7a6a49d9356a7c6cb9865729d4678
Ahmedkhan1121 <ahmedkhan301270@gmail.com>
2024-12-16 20:20:09 +0500

    shopping file in to home group

--- src/components/H2SignUpSearchBar/H2SignUpSearchBar.tsx
+++ src/components/H2SignUpSearchBar/H2SignUpSearchBar.tsx
@@ -1,15 +1,23 @@
+import Form from 'next/form'
 import React from 'react'
 
 function H2SignUpSearchBar() {
   return (
-  <form action="" className='flex justify-center items-center pt-[55px] pb-[65px]'>
+    <>
+  {/* <form action="" className='flex justify-center items-center pt-[55px] pb-[65px]'>
 
       <input className='bg-[#F9F9F9] p-5 w-[354px] py-4 ' type="text" name="" id="" placeholder='your@email.com'/>
   
     
       <button className='bg-[#2A254B] text-white py-4 px-[18px] '>Sign up</button>
     
-  </form>
+  </form> */}
+
+    <Form action=''  className='flex justify-center items-center pt-[55px] pb-[65px]'>
+      <input className='bg-[#F9F9F9] p-5 w-[354px] py-4 outline-none '  required type="text" name="" id="" placeholder='your@email.com'/>
+      <button type='submit' className='bg-[#2A254B] text-white py-3 px-[18px] '>Sign up</button>
+      </Form>
+  </>
   )
 }
 
--- src/components/SignUpSearchBar/SignUpSearchBar.tsx
+++ src/components/SignUpSearchBar/SignUpSearchBar.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Form from 'next/form'
 
 function SignUpSearchBar() {
   return (
-  <form action="" className='flex justify-center items-center pt-[80px] pb-[60px]'>
+    <>
+  {/* <form action="" className='flex justify-center items-center pt-[80px] pb-[60px]'>
 
   
       <input className='bg-[#F9F9F9] p-5 w-[354px] py-3 outline-none' required type="text" name="" id="" placeholder='your@email.com'/>
@@ -10,8 +12,13 @@ function SignUpSearchBar() {
     
       <button className='bg-[#2A254B] text-white py-3 px-[18px] '>Sign up</button>
     
+  </form> */}
 
-  </form>
+  <Form action=''className='flex justify-center items-center pt-[80px] pb-[60px]'>
+    <input className='bg-[#F9F9F9] p-5 w-[354px] py-3 outline-none' required type="text" name="" id="" placeholder='your@email.com'/>
+    <button type='submit' className='bg-[#2A254B] text-white py-3 px-[18px] '>Sign up</button>
+    </Form>
+  </>
   )
 }
 
